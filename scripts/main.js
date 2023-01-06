c7c245105dfcdc706c3c2b1985b8a64479514d69
Braden Huttash <hut21013@byui.edu>
2023-01-06 13:35:23 -0700

    starting portal page

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector('h2').innerHTML = '&copy' + year;
+
+let currentdate = document.lastModified;
+
+document.querySelector('div').textContent = `Last Updated: ${currentdate}`;
