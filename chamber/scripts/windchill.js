617c832414b3b3f89dfb0e6f2ca563cd4c4c4899
Braden Huttash <hut21013@byui.edu>
2023-02-10 19:27:26 -0700

    finished responsive images and wind chill

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,16 @@
+// Pull temp and wind speed from page, and declare variable for wind chill
+let temp = parseFloat(document.getElementById('temp').textContent);
+let speed = parseFloat(document.getElementById('speed').textContent);
+let chill;
+
+// Check if wind chill needs to be calculated
+if (temp <= 50) {
+    if (speed > 3) {
+    // Calculate wind chill
+    chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(speed,0.16)) + (0.4275*temp*Math.pow(speed,0.16)));
+}} else {
+    chill = 'N/A';
+}
+
+// Push wind chill value to span in document
+document.getElementById('chill').textContent = chill + ' °F';
