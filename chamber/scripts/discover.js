d0a18ca2e9bc0e6f28f4d095f6343b72cf9f43a2
Braden Huttash <hut21013@byui.edu>
2023-02-16 14:18:43 -0700

    completed discover page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,4 +1,43 @@
+// Lazy load
+const images = document.querySelectorAll('[data-src]');
+
+function preloadImage(img) {
+    const source = img.getAttribute('data-src');
+    if (!source) {
+        return;
+    }
+    img.src = source;
+}
+
+const io = new IntersectionObserver (
+    (entries) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
+            }
+            console.log(entries);
+        })
+    }, 
+    {threshold: .5}
+);
+
+images.forEach(img => {
+    io.observe(img);
+})
+
 //Calculate days since last visit
 
 // Local Storage
-localStorage.setItem('lastVisit', )
+if (!localStorage.getItem('lastVisit')) {
+    localStorage.setItem('lastVisit', Date.now());
+    document.getElementById('lastVisit').textContent = 'Welcome to the Discover page!';
+} else {
+    let currentDate = Date.now();
+    let lastDate = localStorage.getItem('lastVisit');
+    let daysSince = (currentDate - lastDate) / 1000 / 60 / 60 / 24;
+    document.getElementById('days').textContent = Math.floor(daysSince);
+    localStorage.setItem('lastVisit', currentDate);
+}
