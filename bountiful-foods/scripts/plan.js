d56d0c9945c1d9911e58538fda7a0ad7f665cdea
Braden Huttash <hut21013@byui.edu>
2023-03-22 10:48:30 -0600

    starting bountiful foods site plan

---
+++ bountiful-foods/scripts/plan.js
@@ -0,0 +1,8 @@
+// Hamburger Button
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+})
